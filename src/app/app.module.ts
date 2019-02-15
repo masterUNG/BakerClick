5cdd0726c9347f35c54b05d5a4d2da9965adfa77
masterUNG <phrombutr@gmail.com>
2019-02-15 14:27:33 +0700

    nav OK

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BannertopComponent } from './bannertop/bannertop.component';
+import { ContactmeComponent } from './contactme/contactme.component';
+import { LogoComponent } from './logo/logo.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BannertopComponent,
+    ContactmeComponent,
+    LogoComponent,
+    ToolbarComponent
   ],
   imports: [
     BrowserModule,
